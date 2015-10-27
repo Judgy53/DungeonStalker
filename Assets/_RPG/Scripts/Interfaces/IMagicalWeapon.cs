1a54d36bef6a60d0599cc421735bfcc475adfd03
Judgy53 <alexandre.gaubert94@orange.fr>
2015-10-27 17:01:39 +0100

    Add magic Weapons

---
+++ Assets/_RPG/Scripts/Interfaces/IMagicalWeapon.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface IMagicalWeapon : IWeapon
+{
+    float MinDamages { get; set; }
+    float MaxDamages { get; set; }
+
+    float MaxChargeTime { get; set; }
+    float CurrentChargeTime { get; }
+
+    MagicalWeaponUseState UseState { get; }
+}
+
+public enum MagicalWeaponUseState
+{
+    Default,
+    Charging,
+    Launching
+}
---
+++ Assets/_RPG/Scripts/Weapons/MagicProjectiles/MagicProjectileFireball.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MagicProjectileFireball : MagicProjectile
+{
+
+    [SerializeField]
+    private float summonDistance = 2.0f;
+
+    [SerializeField]
+    private float speed = 10.0f;
+
+    [SerializeField]
+    private float minDamage = 2.0f;
+
+    [SerializeField]
+    private float maxDamage = 10.0f;
+
+    private void Start()
+    {
+        transform.position = launcher.transform.position + launcher.transform.forward * summonDistance;
+        transform.rotation = launcher.transform.rotation;
+
+        transform.localScale += transform.localScale * Power;
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position += transform.forward * speed * Time.fixedDeltaTime;
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject == launcher)
+            return;
+
+        IDamageable target = collider.gameObject.GetComponent<IDamageable>();
+
+        if (target != null)
+        {
+            target.AddDamage(minDamage + ((maxDamage - minDamage) * Power));
+            //target.AddEffect(EffectType.Burn, 10);
+        }
+
+        Destroy(gameObject);
+    }
+}
