bb108c4dbbbf362e0344c06e23b4358e00fa7956
Judgy53 <alexandre.gaubert94@orange.fr>
2015-10-30 11:19:27 +0100

    Add Compass

---
+++ Assets/_RPG/UI/Scripts/UICompass.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UICompass : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject target = null;
+
+	// Use this for initialization
+	private void Start () {
+        if (target == null)
+            Debug.LogWarning("Compass' target not set.");
+	}
+	
+	// Update is called once per frame
+	private void Update () {
+	    if(target != null)
+        {
+            float angle = target.transform.eulerAngles.y;
+
+            float angleRatio = angle / 360f;
+
+            RawImage image = GetComponent<RawImage>();
+
+            Rect rect = image.uvRect;
+            rect.x = angleRatio;
+            image.uvRect = rect;
+        }
+	}
+}
