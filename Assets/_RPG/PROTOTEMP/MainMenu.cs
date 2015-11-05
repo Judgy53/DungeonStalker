cf5ec9b714930a1b6765513aa41a65a96a9d6d22
Pierre-Julien Baron <p.baron@A-T1700-12-WIN.isart.lan>
2015-11-05 15:04:57 +0100

    Added back buttons

--- Assets/_RPG/PROTOTEMP/MainMenu.cs
+++ Assets/_RPG/PROTOTEMP/MainMenu.cs
@@ -1,11 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MainMenu : MonoBehaviour {
-
+public class MainMenu : MonoBehaviour
+{
     public void LoadScene(int id)
     {
         Application.LoadLevel(id);
+    }
+
+    public void LoadScene(string name)
+    {
+        Application.LoadLevel(name);
     }
 
     public void Quit()
