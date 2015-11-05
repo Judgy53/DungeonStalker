0878a4bb15ad8c4e8154a2a2651b46c6fa01b875
Judgy53 <alexandre.gaubert94@orange.fr>
2015-11-05 14:08:37 +0100

    Added Main Menu. Fix seed input in maze generator.

---
+++ Assets/_RPG/PROTOTEMP/MainMenu.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+    public void LoadScene(int id)
+    {
+        Application.LoadLevel(id);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
