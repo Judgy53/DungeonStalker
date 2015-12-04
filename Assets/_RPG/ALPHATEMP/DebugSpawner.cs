de0f7d292823f31701f1dea0d78e80bf65227c3f
Pierre-Julien Baron <p.baron@A-T1700-12-WIN.isart.lan>
2015-12-04 16:07:51 +0100

    Alpha temp script debugger, scene cleaning.

---
+++ Assets/_RPG/ALPHATEMP/DebugSpawner.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DebugSpawner : MonoBehaviour
+{
+    public GameObject[] itemPrefabs = new GameObject[0];
+
+    private GameObject player = null;
+
+    private void Start()
+    {
+        GameManager.OnPlayerCreation += GameManager_OnPlayerCreation;
+    }
+
+    private void GameManager_OnPlayerCreation(object sender, EventPlayerCreationArgs e)
+    {
+        player = e.player;
+    }
+
+    public void SpawnCallback(int value)
+    {
+        if (player != null)
+            GameObject.Instantiate(itemPrefabs[value], player.transform.position + player.transform.forward * 0.2f, Quaternion.identity);
+    }
+}
