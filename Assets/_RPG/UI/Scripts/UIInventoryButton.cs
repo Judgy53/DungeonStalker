ec49fe25cd5f30e61477da060b14dd56e8d0bf24
Judgy53 <alexandre.gaubert94@orange.fr>
2015-12-11 11:57:51 +0100

    Fix Enemies spawning in same room as player. Fix Death not working after stage 1. Forgot to bind Drop key on new inventory. Added a help for buttons in inventory.

---
+++ Assets/_RPG/UI/Scripts/UIInventoryButton.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIInventoryButton : MonoBehaviour {
+
+    private IItem item;
+    public IItem Item { get { return item; } set { item = value; } }
+    
+}
